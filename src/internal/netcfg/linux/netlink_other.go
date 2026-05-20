ebc34dfbb908da1fade73e6edb7077d0d0f76d76
N4darae(Khanh) <traxanhvichanh2014@gmail.com>
2026-05-20 13:29:19 +0700

    move the rtnetlink socket behind a build tag

---
+++ src/internal/netcfg/linux/netlink_other.go
@@ -0,0 +1,18 @@
+//go:build !linux
+
+package linux
+
+import (
+	"context"
+
+	"github.com/n4darae/huawei-API/src/internal/domain"
+	"github.com/n4darae/huawei-API/src/internal/netcfg"
+)
+
+func dump(uint16, []byte) ([]nlMsg, error) {
+	return nil, domain.UnsupportedOn("rtnetlink dumps")
+}
+
+func (o *Observer) Subscribe(context.Context) (<-chan netcfg.LinkEvent, func(), error) {
+	return nil, nil, domain.UnsupportedOn("rtnetlink link event subscription")
+}
